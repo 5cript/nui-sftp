4679300fde09658b8bd1eb2a77ad820244854844
5cript <tim06tr@gmail.com>
2026-02-23 20:09:13 +0100

    Added windows workflow.

---
+++ _cmake/version.cmake
@@ -0,0 +1,56 @@
+find_package(Git REQUIRED)
+execute_process(
+    COMMAND
+        "${GIT_EXECUTABLE}" rev-list --tags --max-count=1
+    OUTPUT_VARIABLE
+        REV_LIST
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+execute_process(
+    COMMAND
+        "${GIT_EXECUTABLE}" describe --tags --dirty
+    OUTPUT_VARIABLE
+        VERSION
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+execute_process(
+    COMMAND
+        "${GIT_EXECUTABLE}" describe --tags ${REV_LIST}
+    OUTPUT_VARIABLE
+        VERSION_NON_DIRTY
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+#if tag starts with "v" remove it
+if ("${VERSION}" MATCHES "^v.+")
+    string(SUBSTRING "${VERSION}" 1 -1 VERSION)
+endif()
+if ("${VERSION_NON_DIRTY}" MATCHES "^v.+")
+    string(SUBSTRING "${VERSION_NON_DIRTY}" 1 -1 VERSION_NON_DIRTY)
+endif()
+
+if("${VERSION}" STREQUAL "")
+  set(GIT_TAG "N/A")
+  set(GIT_BRANCH "N/A")
+  set(VERSION "0.0.0")
+  set(VERSION_NON_DIRTY "0.0.0")
+else()
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} describe --exact-match --tags OUTPUT_VARIABLE GIT_TAG ERROR_QUIET)
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD OUTPUT_VARIABLE GIT_BRANCH)
+
+  string(STRIP "${GIT_TAG}" GIT_TAG)
+  string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
+  string(STRIP "${VERSION}" VERSION)
+  string(STRIP "${VERSION_NON_DIRTY}" VERSION_NON_DIRTY)
+endif()
+
+configure_file(
+  "${CMAKE_CURRENT_LIST_DIR}/configured_files/version.hpp.in"
+  "${CMAKE_BINARY_DIR}/include/version.hpp"
+)
+configure_file(
+  "${CMAKE_CURRENT_LIST_DIR}/configured_files/version.txt.in"
+  "${CMAKE_BINARY_DIR}/text/version.txt"
+)
