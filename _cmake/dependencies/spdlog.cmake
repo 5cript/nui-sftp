a840461c100c8d14262726cce2566b0a1d522542
Tim Ebbeke <Tim06TR@gmail.com>
2026-03-02 00:54:28 +0100

    Bumped spdlog version.

--- _cmake/dependencies/spdlog.cmake
+++ _cmake/dependencies/spdlog.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
     spdlog
     GIT_REPOSITORY https://github.com/gabime/spdlog.git
-    GIT_TAG        v1.16.0
+    GIT_TAG        v1.17.0
 )
 
 FetchContent_MakeAvailable(spdlog)
