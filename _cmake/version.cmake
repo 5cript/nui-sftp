f14682e326b8b7d57d68a3e94a791fffea29e32b
5cript <tim06tr@gmail.com>
2026-03-02 19:47:53 +0100

    Fixed CMake version script.

--- _cmake/version.cmake
+++ _cmake/version.cmake
@@ -33,32 +33,50 @@ if ("${VERSION_NON_DIRTY}" MATCHES "^v.+")
 endif()
 
 if("${VERSION}" STREQUAL "")
-  set(GIT_TAG "N/A")
-  set(GIT_BRANCH "N/A")
-  set(VERSION "0.0.0")
-  set(VERSION_NON_DIRTY "0.0.0")
-  message(WARNING "Git information not found, using default version 0.0.0")
+    set(GIT_TAG "N/A")
+    set(GIT_BRANCH "N/A")
+    set(VERSION "0.0.0")
+    set(VERSION_NON_DIRTY "0.0.0")
+    set(PROJECT_VERSION "0.0.0")
+    message(WARNING "Git information not found, using default version 0.0.0")
 else()
-  execute_process(
-    COMMAND ${GIT_EXECUTABLE} describe --exact-match --tags OUTPUT_VARIABLE GIT_TAG ERROR_QUIET)
-  execute_process(
-    COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD OUTPUT_VARIABLE GIT_BRANCH)
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} describe --exact-match --tags OUTPUT_VARIABLE GIT_TAG ERROR_QUIET)
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD OUTPUT_VARIABLE GIT_BRANCH)
 
-  string(STRIP "${GIT_TAG}" GIT_TAG)
-  string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
-  string(STRIP "${VERSION}" VERSION)
-  string(STRIP "${VERSION_NON_DIRTY}" VERSION_NON_DIRTY)
-  message(STATUS "Git tag: ${GIT_TAG}")
-  message(STATUS "Git branch: ${GIT_BRANCH}")
-  message(STATUS "Version: ${VERSION}")
-  message(STATUS "Version (non-dirty): ${VERSION_NON_DIRTY}")
+    set(SEMVER_REGEX
+        "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(-((0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(\\.(0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\\+([0-9a-zA-Z-]+(\\.[0-9a-zA-Z-]+)*))?$"
+    )
+
+    string(REGEX MATCH "${SEMVER_REGEX}" _ "${VERSION_NON_DIRTY}")
+    set(SEMVER_MAJOR "${CMAKE_MATCH_1}")
+    set(SEMVER_MINOR "${CMAKE_MATCH_2}")
+    set(SEMVER_PATCH "${CMAKE_MATCH_3}")
+    set(SEMVER_PRERELEASE "${CMAKE_MATCH_5}") # without leading '-'
+    set(SEMVER_BUILD "${CMAKE_MATCH_10}")      # without leading '+'
+    set(PROJECT_VERSION "${SEMVER_MAJOR}.${SEMVER_MINOR}.${SEMVER_PATCH}")
+
+    string(STRIP "${GIT_TAG}" GIT_TAG)
+    string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
+    string(STRIP "${VERSION}" VERSION)
+    string(STRIP "${VERSION_NON_DIRTY}" VERSION_NON_DIRTY)
+    message(STATUS "Git tag: ${GIT_TAG}")
+    message(STATUS "Git branch: ${GIT_BRANCH}")
+    message(STATUS "Version: ${VERSION}")
+    message(STATUS "Version (non-dirty): ${VERSION_NON_DIRTY}")
+    message(STATUS "Semver major: ${SEMVER_MAJOR}")
+    message(STATUS "Semver minor: ${SEMVER_MINOR}")
+    message(STATUS "Semver patch: ${SEMVER_PATCH}")
+    message(STATUS "Semver prerelease: ${SEMVER_PRERELEASE}")
+    message(STATUS "Semver build: ${SEMVER_BUILD}")
 endif()
 
 configure_file(
-  "${CMAKE_CURRENT_LIST_DIR}/configured_files/version.hpp.in"
-  "${CMAKE_BINARY_DIR}/include/version.hpp"
+    "${CMAKE_CURRENT_LIST_DIR}/configured_files/version.hpp.in"
+    "${CMAKE_BINARY_DIR}/include/version.hpp"
 )
 configure_file(
-  "${CMAKE_CURRENT_LIST_DIR}/configured_files/version.txt.in"
-  "${CMAKE_BINARY_DIR}/text/version.txt"
+    "${CMAKE_CURRENT_LIST_DIR}/configured_files/version.txt.in"
+    "${CMAKE_BINARY_DIR}/text/version.txt"
 )
