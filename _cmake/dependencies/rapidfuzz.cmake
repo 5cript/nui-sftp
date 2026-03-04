4c0ed0aa4b647def06c1169ae9415986033541c2
5cript <tim06tr@gmail.com>
2026-03-04 22:27:50 +0100

    Made rapidfuzz and spdlog optional fetches.

--- _cmake/dependencies/rapidfuzz.cmake
+++ _cmake/dependencies/rapidfuzz.cmake
@@ -1,6 +1,9 @@
-FetchContent_Declare(
-    rapidfuzz
-    GIT_REPOSITORY https://github.com/rapidfuzz/rapidfuzz-cpp.git
-    GIT_TAG main
-)
-FetchContent_MakeAvailable(rapidfuzz)
+if (FETCH_RAPIDFUZZ)
+    include(FetchContent)
+    FetchContent_Declare(
+        rapidfuzz
+        GIT_REPOSITORY https://github.com/rapidfuzz/rapidfuzz-cpp.git
+        GIT_TAG b8ce411e91e01599d0697ad307933e05ddf3a723
+    )
+    FetchContent_MakeAvailable(rapidfuzz)
+endif()
--- _cmake/dependencies/spdlog.cmake
+++ _cmake/dependencies/spdlog.cmake
@@ -1,10 +1,12 @@
 set(SPDLOG_FMT_EXTERNAL ON CACHE BOOL "Use an external fmt library (provide it manually)" FORCE)
 
-include(FetchContent)
-FetchContent_Declare(
-    spdlog
-    GIT_REPOSITORY https://github.com/gabime/spdlog.git
-    GIT_TAG        v1.17.0
-)
+if (FETCH_SPDLOG)
+    include(FetchContent)
+    FetchContent_Declare(
+        spdlog
+        GIT_REPOSITORY https://github.com/gabime/spdlog.git
+        GIT_TAG        v1.17.0
+    )
 
-FetchContent_MakeAvailable(spdlog)
+    FetchContent_MakeAvailable(spdlog)
+endif()
