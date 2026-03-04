58e1ab4bff98c09e1f734c9d75a1c8c346a9955a
5cript <tim06tr@gmail.com>
2026-03-04 22:36:43 +0100

    Made efsw fetch optional.

--- _cmake/dependencies/efsw.cmake
+++ _cmake/dependencies/efsw.cmake
@@ -1,8 +1,10 @@
-include(FetchContent)
-FetchContent_Declare(
-    efsw
-    GIT_REPOSITORY https://github.com/SpartanJ/efsw.git
-    GIT_TAG        87abe599995d5646f5d83cf2e3a225bd73148b3a
-)
+if (FETCH_EFSW)
+    include(FetchContent)
+    FetchContent_Declare(
+        efsw
+        GIT_REPOSITORY https://github.com/SpartanJ/efsw.git
+        GIT_TAG        87abe599995d5646f5d83cf2e3a225bd73148b3a
+    )
 
-FetchContent_MakeAvailable(efsw)
+    FetchContent_MakeAvailable(efsw)
+endif()
