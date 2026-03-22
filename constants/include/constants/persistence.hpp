12bfe3964407f71569347dde7461e6b5e24d8808
5cript <tim06tr@gmail.com>
2026-03-22 13:35:30 +0100

    Split config path between develop and release.
    For stable use installation and development.

--- constants/include/constants/persistence.hpp
+++ constants/include/constants/persistence.hpp
@@ -4,6 +4,10 @@
 
 namespace Constants
 {
+#ifdef NDEBUG
     constexpr static std::string_view persistencePath = "%config_home2%/nui-sftp/persistence.json";
+#else
+    constexpr static std::string_view persistencePath = "%config_home2%/nui-sftp/persistence.devel.json";
+#endif
     constexpr static std::string_view appName = "nui-sftp";
 }
