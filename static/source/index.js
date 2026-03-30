320d089dc7a69f4ea068388fea47d8838180a61d
5cript <tim06tr@gmail.com>
2026-03-30 23:24:53 +0200

    Fixed unicode encoding issues.

--- static/source/index.js
+++ static/source/index.js
@@ -12,4 +12,8 @@ import { ContentPanelManager } from "./content_panel_manager.ts";
         const alpha = getAlpha(colorString);
         return { r: rgb.r, g: rgb.g, b: rgb.b, a: alpha * 255 };
     };
+
+    globalThis.decodeUtf8Base64 = (base64) => {
+        return new TextDecoder().decode(Uint8Array.fromBase64(base64));
+    }
 })();
