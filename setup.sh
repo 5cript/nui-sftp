f527251495fbd2bdf45d554332227fa8c2df3307
5cript <tim06tr@gmail.com>
2026-04-22 19:10:08 +0200

    Updated workflows for submodules.

--- setup.sh
+++ setup.sh
@@ -2,12 +2,4 @@
 
 set -e
 
-# Download dependency cloner
-mkdir -p dependencies/work-dependency-cloner
-git clone https://github.com/5cript/work-dependency-cloner.git dependencies/work-dependency-cloner || echo "Dependency cloner already cloned."
-
-# Create a "build dir"
-mkdir -p build/dependency-cloner
-cmake -S dependencies/work-dependency-cloner -B build/dependency-cloner
-cmake --build build/dependency-cloner --target clean
-cmake --build build/dependency-cloner
+git submodule update --init --recursive
